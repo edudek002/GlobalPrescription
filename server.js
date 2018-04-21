053496e1c0656d0560d4e56623600cdcd4aec19b
Your Name <nbos0531@gmail.com>
2018-04-21 13:36:08 -0500

    server revision

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const axios = require("axios");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static("client/public"));
+app.use(express.static("client/build"));
 // Add routes, both API and view
 //app.use(routes);
 
