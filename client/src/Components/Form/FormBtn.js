fdcb1e51d58a9f484de4f72fcaa3c09ad6a9863d
Your Name <nbos0531@gmail.com>
2018-04-23 18:06:03 -0500

    delete folder

--- client/src/Components/Form/FormBtn.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const FormBtn = props => (
-  <button {...props} style={{ float: "center", marginBottom: 10 }} className="btn btn-lg btn-dark">
-    {props.children}
-  </button>
-);
