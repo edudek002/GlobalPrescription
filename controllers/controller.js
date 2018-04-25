45e77290b5f679d8b7c7e02ffb20893025c10ee9
nbos17 <nbos0531@gmail.com>
2018-04-24 21:37:22 -0500

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,7 +5,7 @@ module.exports = {
 
   findAll: function(req, res) {
     db.Drug
-      .find({active_ingredient : "test2"})
+      .find()
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
