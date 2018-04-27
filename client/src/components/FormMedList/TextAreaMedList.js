8f345d81dac5fecbd46cfb92b1eaa337f2c243c9
Your Name <nbos0531@gmail.com>
2018-04-26 21:27:40 -0500

    removing components

--- client/src/components/FormMedList/TextAreaMedList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const TextAreaMedList = props => (
-  <div className="form-group">
-    <textarea className="form-control" rows="3" {...props} />
-  </div>
-);
--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import Reactstrap from "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
