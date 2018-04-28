b3faf4bbbc6748a77d9ed9126f834895d2925ed6
kimonjo67 <monjokim@gmail.com>
2018-04-28 10:34:12 -0500

    log in bootsraped

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,22 +1,13 @@
 import React from "react";
 import "./jumbotron.css";
 
-// const Jumbotron = ({ children }) => (
-//   <div
-//     style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-//     className="jumbotron"
-//   >
-//     {children}
-//   </div>
-// );
-
 const Jumbotron = () => (
-  <div className="jumbotron jumbotron-fluid">
+  // <div className="jumbotron jumbotron-fluid">
     <div className="container">
       <h1 className="display-4">GLOBAL PRESCRIPTION</h1>
-      <p className="lead">GLOBAL PRESCRIPTION.</p>
+      <p className="lead">Prescptions on the Go!.</p>
     </div>
-  </div>
+  // </div>
 );
 
 export default Jumbotron;
