c583f25b0353fefc7e25fdd0b3a70c29fe126e5f
Your Name <nbos0531@gmail.com>
2018-05-01 18:14:20 -0500

    search bar changes

--- client/src/components/MapBtn/MapBtn.js
+++ client/src/components/MapBtn/MapBtn.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import "./MapBtn.css";
 
 class MapBtn extends React.Component {
   render() {
     return (
       <Form inline>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label for="examplesearch" className="mr-sm-2"></Label>
+          <Label for="examplesearch" id="searches" className="mr-sm-2"></Label>
           <Input type="search" name="search" id="examplesearch" placeholder="Enter location (City)" />
         </FormGroup>
         <Button>Submit</Button>
