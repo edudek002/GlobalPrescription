b934082993ba5503a11afe52a6e4e55f09d02494
Your Name <nbos0531@gmail.com>
2018-05-01 19:37:45 -0500

    added new page routes

---
+++ client/src/components/UserMedList/UserMedList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./UserMedList.css"
+
+
+const UserMedList = (props) => (
+  // <div className="jumbotron jumbotron-fluid">
+    <div className="test">
+      <h1 className="display-4">GLOBAL PRESCRIPTION</h1>
+      <p className="lead">Prescptions on the Go!.</p>
+    </div>
+
+);
+
+
+export default UserMedList;
