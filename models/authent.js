879d94dce15ee3b5c4cfc679a2b54c2e3aa54112
Your Name <nbos0531@gmail.com>
2018-05-06 00:55:53 -0500

    rounded edge

--- models/authent.js
+++ models/authent.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    userName: { type: String, required: true },
+    userName: { type: String, required: true, unique : true },
     passWord: { type: String, required: true },
     userDrugs: {type: Array, required: false}
     
