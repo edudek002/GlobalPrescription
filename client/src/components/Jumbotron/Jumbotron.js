7ae4ad0e0a6ce739bc33d66dd38f4273b1d4689f
kimonjo67 <monjokim@gmail.com>
2018-05-06 16:54:45 -0500

    edited homepage

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ const Jumbotron = (props) => (
   // <div className="jumbotron jumbotron-fluid">
     <div className="container">
       <h1 className="display-4">GLOBAL PRESCRIPTION</h1>
-      <p className="lead">Prescptions on the Go!.</p>
+      {/* <p className="lead">Prescptions on the Go!.</p> */}
       <h2>Welcome : {props.activeUser} </h2>
     </div>
   // </div>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,30 +5,32 @@ const Footer = () => (
   <nav className="navbar-expand-lg navbar-dark navbar-fixed-bottom">
     <div className="container">
       {/* <div className="row"> */}
-        <div className="row">
-          <div class="col-md-12">
-            <br />
-            <br />
-            <h2>JOIN THE GLOBAL PRESCRIPTION COMMUNITY</h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <p>Created by Drugs Rx Us</p>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-12">
-            <p>
-              Nathan Bos, Walter Zuschlag, Edyta Dudek, Amin Kedir, Sam Mwangi
-            </p>
-          </div>
-        </div>
-        <div className="row">
-          <p class="mx-auto">&copy;Copyright 2018.</p>
+      <div className="row">
+        <div className="col-md-12">
+          <br />
+          <br />
+          <h2>JOIN THE GLOBAL PRESCRIPTION COMMUNITY</h2>
         </div>
       </div>
+      <div className="row">
+        <div className="col-md-12">
+          <p>Created by Drugs Rx Us</p>
+        </div>
+      </div>
+
+      
+
+      <div className="row">
+        <div className="col-md-12">
+          <p>
+            Nathan Bos, Walter Zuschlag, Edyta Dudek, Amin Kedir, Sam Mwangi
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <p className="mx-auto">&copy;Copyright 2018.</p>
+      </div>
+    </div>
     {/* </div> */}
   </nav>
 );
